3853e5429e238c36d92b9c04ef2c99bfad179daa
Jessica Ledezma <itsjessicat99@gmail.com>
2026-05-27 23:59:30 -0700

    Agrega mensaje al boton principal

--- SistemaEscolar3/Form1.cs
+++ SistemaEscolar3/Form1.cs
@@ -19,7 +19,7 @@ namespace SistemaEscolar3
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Es la pantalla principal");
         }
     }
 }
