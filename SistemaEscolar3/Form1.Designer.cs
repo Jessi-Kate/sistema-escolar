cdcda5c6f330e0e8ca066e040d1aee6bf7bfd782
Jessica Ledezma <itsjessicat99@gmail.com>
2026-05-28 00:03:20 -0700

    Cambia fondo del formulario principal

--- SistemaEscolar3/Form1.Designer.cs
+++ SistemaEscolar3/Form1.Designer.cs
@@ -45,6 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
